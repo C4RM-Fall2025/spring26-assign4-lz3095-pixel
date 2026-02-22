fee824af361260fcc2b815e14def6fcc501f314c
lz3095-pixel <lz3095@columbia.edu>
2026-02-21 19:33:46 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,17 @@
-
-
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    coupon = face * couponRate
+    bondPrice = 0
+
+    for i, y in enumerate(yc):
+        t=i+1
+
+        cf=coupon
+        if t==m:
+            cf=cf+face
+
+        pvm=1/((1+y)**t)
+        pvcf=pvm*cf
+        
+        bondPrice=bondPrice+pvc
+
+    return bondPrice
